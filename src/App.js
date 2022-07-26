f457ccd46bcd9548b703f62d40781b6523052157
Amir <a.em@outlook.com>
2022-07-26 14:25:25 +0200

    put geolocation outside render() and change state with setState

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+  // the first function which is called 
   constructor(props) {
     super(props)
+
     this.state = { lat: null }
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => console.log(err)
+    )
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    )
     return (<div>Latitude: {this.state.lat}</div>);
   }
 }
