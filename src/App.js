3a74940e2d614501dfcd94088599d6806500451a
Amir <a.em@outlook.com>
2022-07-26 14:15:04 +0200

    create state in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = {}
+
+  constructor(props) {
+    super(props)
+    this.state = { lat: null }
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       position => console.log(position),
       err => console.log(err)
     )
-    return (<div>Latitude</div>);
+    return (<div>Latitude: {this.state.lat}</div>);
   }
 }
 
