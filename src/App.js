6725af45147559330ef1c1010e2b316993a98469
Amir <a.em@outlook.com>
2022-07-26 14:45:08 +0200

    setting error message

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { lat: null }
+    this.state = { lat: null, errorMessage: '' }
 
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
-      err => console.log(err)
+      err => this.setState({ errorMessage: err.message })
     )
   }
 
   render() {
-    return (<div>Latitude: {this.state.lat}</div>);
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>{this.state.errorMessage}</div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>{this.state.lat}</div>
+    }
+    return <div>Loading ...</div>
   }
 }
 
