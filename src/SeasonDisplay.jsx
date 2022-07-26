eb6c287b8e506709ceb983ab97ba06e907f80b21
Amir <a.em@outlook.com>
2022-07-26 16:02:01 +0200

    add findSeason()

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-     return (<div></div>);
-}
+const findSeason = (lat, month) => {
+     if (month > 2 && month < 9) {
+          return lat > 0 ? 'Summer' : 'Winter';
+     } else {
+          lat > 0 ? 'Winter' : 'Summer';
+     }
+};
+
+
+const SeasonDisplay = props => {
+
+     const season = findSeason(props.lat, new Date().getMonth());
+
+     return (<div>Season in your location is: {season}</div>);
+};
 
 export default SeasonDisplay;
