a825d39706b6df80e76a88faa7a0e4d01ab5a50a
Amir <a.em@outlook.com>
2022-07-27 00:33:48 +0200

    add loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SeasonDisplay from './SeasonDisplay';
+import Loading from './Loading';
 
 class App extends Component {
 
@@ -21,7 +22,7 @@ class App extends Component {
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading ...</div>
+    return <Loading />
   }
 }
 
